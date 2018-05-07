9847d339bd320cf03e953a2b288efc9eb4a97ceb
sfaulhammer <s.faulhammer@gmx.at>
2018-05-07 22:36:47 +0200

    [06] restart

--- project/index.php
+++ project/index.php
@@ -11,6 +11,8 @@ use sfaulhammer\baelle\Fussball;
 use sfaulhammer\baelle\Basketball;
 use sfaulhammer\baelle\PingPongBall;
 
+require_once "vendor/autoload.php";
+
 $fussball = new Fussball("Adidas Sport 1000", 30.75, "Gummi");
 $fussball2 = new Fussball("Nike Sport Elite", 30.75, "Gummi");
 $basketball = new Basketball("NBA Pro Elite", 25.7, "Kautschuk");
